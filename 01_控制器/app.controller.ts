55f1182628e4be934824a3e9ad3ca39044bf58d5
ZAM <p602985@yeah.net>
2022-10-25 17:46:49 +0800

    demo

---
+++ 01_控制器/app.controller.ts
@@ -0,0 +1,7 @@
+import { Body, Controller, Get, Header, HttpCode, Param, Post, Redirect } from '@nestjs/common'
+import { AppService } from './app.service'
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+}
